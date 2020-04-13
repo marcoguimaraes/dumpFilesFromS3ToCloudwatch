b77e258668993811972a1b52e9ec5203e16e68a4
marcoguimaraes <63541026+marcoguimaraes@users.noreply.github.com>
2020-04-13 17:08:51 +0100

    Add files via upload

---
+++ dumpFilesFromS3ToCloudwatch.py
@@ -0,0 +1,43 @@
+import json
+import urllib.parse
+import boto3
+
+s3 = boto3.client('s3')
+
+def s3FileRead(fileObject):
+    try:
+        print('Start of file read')
+        print(fileObject['Body'].read().decode('utf-8'))
+        print('End of file read')
+    except Exception as e:
+        print(e)
+        raise e
+        
+def s3FileDelete(bucket, key):
+    try:
+        print('Deleting file...')
+        s3.delete_object(Bucket=bucket, Key=key)
+        print('File deleted!')
+    except Exception as e:
+        print(e)
+        raise e
+
+def s3FileUploadEvent(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    try:
+        # Get the object from the event and show its content type
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+        print('File "' + key + '" received in S3 bucket "' + bucket + '"')        
+        fileObject = s3.get_object(Bucket=bucket, Key=key)
+        contentType = fileObject['ContentType']
+        if not contentType.startswith('text/'):
+            print('Content Type ' + contentType + ' not allowed, only text files')
+        else:
+            s3FileRead(fileObject)
+        s3FileDelete(bucket, key)
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
